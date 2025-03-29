75ec898d9cd927b198c0ea1af7c499e6344ff545
javahir1223 <javah1223@gmail.com>
2025-03-29 16:41:27 +0500

    init commit

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true }, // Добавил unique
-    email: { type: String, required: true, unique: true }, // Добавил unique
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true }, 
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: true },
   },
